806838096e0ada0f8913eb91d1f59e933148e96c
velvi-stud <romeo.velvi001@studenti.uniparthenope.it>
2021-09-01 12:30:49 +0200

    update

---
+++ LABORATORIO/programmi/Esercitazione biblioteca/Esercizio semafori/uno.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define N 15 /* scrivere numeri da 1 a 15 */
+
+int fdfile; // fd file
+sem_t semprod, semcons; // semafori
+
+void produce(){
+	int cont=0;
+	while(cont <= N){
+		lseek(fdfile,0,SEEK_SET); // piazzati all'inizio
+		sem_wait(&semprod);
+		write(fdfile,&cont,sizeof(int));
+		printf("\n Sono produttore (%d) e scrivo: %d",getpid(),cont);
+		sem_post(&semcons);
+		cont++;
+	}
+}
+
+void consume(){
+	int cont=0;
+	while(cont != N){
+		if(lseek(fdfile,0,SEEK_SET)==-1); // piazzati all'inizio
+		sem_wait(&semcons);
+		read(fdfile,&cont,sizeof(int));
+		printf("\n Sono consumatore (%d) e leggo: %d",getpid(),cont);
+		sem_post(&semprod);
+	}
+}
+
+int main(){
+	
+	fdfile = open("prova.txt", O_RDWR | O_CREAT | O_TRUNC, 0777);
+	
+	sem_init(&semprod, 1, 1);
+	sem_init(&semcons, 1, 0);
+
+
+	if( write(fdfile,&fdfile,sizeof(fdfile)) != sizeof(fdfile)){
+		printf("write error");
+		exit(0);
+	}
+
+	/* filgio produttore */	
+	if(fork()==0){
+		produce();
+		exit(1);
+	}
+	/* padre consumatore */
+	else{
+		consume();
+	}
+
+	printf("\nFINE PROGRAMMA\n");
+		
+	return 0;
+}
+
+
+
+
--- LABORATORIO/programmi/slide6-7/Creazione e attesa processi/uno.c
+++ LABORATORIO/programmi/slide6-7/Creazione e attesa processi/uno.c
@@ -6,50 +6,49 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include "apue.h" 
 #include <sys/wait.h>
 
 void funexit(void){
-    printf("\nUSCITA %d\n", getpid());
+    printf("\n (funzione) USCITA %d\n", getpid());
     return;
 }
 
-void status(){printf("\n processo %d, padre %d",getpid(),getppid());}
-void status2(){printf("\nCREAZIONE sta il processo %d, padre %d",getpid(),getppid());}
 
 int main (int argc, char *argv[]) { 
-if (argc != 2){ /* controllo argomenti */ 
-    fprintf(stderr, "Uso: %s processi\n", argv[0]); 
-    return 1;
-}
 
-atexit(funexit);
-printf("\nSTART: %d",getpid());
-int i, n;
-pid_t childpid;
-pid_t waitreturn;
-pid_t flag;
-n = atoi(argv[1]);
-
-for (i = 1; i <= n; i++){
-status2();
-    printf("\n%d: prima dalla fork",i);
-    childpid = fork();
-    printf("\n%d: eseguo fork, esito: %d",i, childpid);
-    if (childpid == 0){
-        printf("\nBlocco istruzioni processo: %d", getpid());
-        break;
+    if (argc != 2){ /* controllo argomenti */ 
+        fprintf(stderr, "Uso: %s processi\n", argv[0]); 
+        return 1;
     }
-}
 
-//printf("\n sono %d e aspetto: %d",getpid(), waitreturn);
-status();
-if(argc != 0)
-while((childpid != (waitreturn = wait(NULL)))){
-printf("\nsono: %d aspetto: %d", getpid(), waitreturn);
-    if (waitreturn == -1)
-        break;
-}
+    /* setta funzione di uscita */
+    atexit(funexit);
 
-exit(0);
+    printf("\nSTART: %d",getpid());
+    int i, n;
+    pid_t childpid;
+    pid_t waitreturn;
+    pid_t flag;
+    n = atoi(argv[1]);
+
+    for (i = 1; i <= n; i++){      
+        if ( (childpid=fork) == 0){
+            printf("\n ...Blocco istruzioni processo: %d (figlio di %d)", getpid(), getppid());
+            break;
+        }else{
+            printf("\n%d: eseguo fork, esito: %d",i, childpid);
+        }
+    }
+
+    /* usciranno i processi creati (ed il padre) */
+
+    if(argc != 0) {
+        while((childpid != (waitreturn = wait(NULL)))){
+        printf("\nsono: %d aspetto: %d", getpid(), waitreturn);
+            if (waitreturn == -1)
+                break;
+        }
+    }
+
+    exit(0);
 }
